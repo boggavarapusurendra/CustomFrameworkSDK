1b9433455bee80a4eb903d439c4aa961bc66427b
BoggavarapuSurendra1 <surendraboggavarapu1@gmail.com>
2024-11-30 14:10:41 +0530

    initial commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,16 @@
+//
+//  SDK.swift
+//  CustomFrameworkProject
+//
+//  Created by Boggavarapu Surendra on 2024-11-30.
+//
+
+import Foundation
+
+
+class SDK {
+    public static func doSomework(){
+        print("Doing some work ... ")
+    }
+}
+ 
